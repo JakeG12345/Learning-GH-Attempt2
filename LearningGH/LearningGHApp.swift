69c45a3e8c2f6b29bf04b2cb07107442203fddb2
Jake Glick <jake@glickpeople.com>
2021-09-29 11:46:53 +1000

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Jake Glick on 29/9/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
